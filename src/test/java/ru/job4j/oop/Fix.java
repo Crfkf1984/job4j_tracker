aa2dc23aa8d1ce1ce1be9c4c0b5db597a9b52e65
Сергей <seirosso1@gmail.com>
2020-12-29 21:01:48 +0300

    1.1.1. Конструктор по умолчанию

---
+++ src/test/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+    public Fix(String name) {
+         desc = name;
+    };
+
+    public static void main(String[] args) {
+        String issue = "Hello World !";
+        Fix item = new Fix(issue);
+    }
+}
