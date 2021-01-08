c123b029c68a034032a5c23822dffefebf333e97
Сергей <seirosso1@gmail.com>
2021-01-08 20:40:02 +0300

    2. Scanner и чтение числа из консоли.

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -3,13 +3,34 @@ import java.util.Scanner;
 
 public class Matches {
     public static void main(String[] args) {
-        int rsl = 11;
+        String name1 = "Андрей";
+        String name2 = "Сергей";
+;        int rsl = 11;
+        int igrock1 = 0;
+        int igrock2 = 0;
         while (rsl > 0) {
-            System.out.println("Берите спички!");
+            System.out.println("Берите спички, " + name1);
             Scanner res = new Scanner(System.in);
             int select = Integer.valueOf(res.nextLine());
-            rsl = rsl - select;
-            System.out.println("Осталось " + rsl);
+            if (select <= 3) {
+                rsl = rsl - select;
+                 igrock1++;
+                System.out.println("Осталось " + rsl);
+            } else {
+                System.out.println("Вы нарушаете правила, ход переходит другому !!!");
+            }
+            System.out.println("Берите спички," + name2);
+            int select1 = Integer.valueOf(res.nextLine());
+            if (select1 <= 3) {
+                rsl = rsl - select1;
+                igrock2++;
+                System.out.println("Осталось " + rsl);
+            } else {
+                System.out.println("Вы нарушаете правила, ход переходит другому !!!");
+            }
         }
+
+        String res = igrock1 > igrock2 ? name1 : name2;
+        System.out.println("Победа присуждается : " + res);
     }
 }
