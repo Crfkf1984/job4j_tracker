7c72d49ebe135ed64ec4aabc6afba8a0b2d6c265
Сергей <seirosso1@gmail.com>
2021-01-12 20:25:34 +0300

    3. Интерфейс Input

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,27 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void go() {
+        int pointa = 23;
+        int pointb = 25;
+        int allway = pointa + pointb;
+    }
+
+    @Override
+    public void passengers(int temp) {
+         int exita = 10;
+         int exitb = 5;
+         int allexit = exita + exitb;
+    }
+
+    @Override
+    public int fillup(int full) {
+        int price;
+        int fulla = 12;
+        int fullb = 10;
+        int cost = 2;
+        int allfull = fulla + fullb;
+        return price = allfull * cost;
+    }
+}
