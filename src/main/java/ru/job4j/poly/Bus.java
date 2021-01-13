cba84f17425bb07650976f9b6db966c7a78a591f
Сергей <seirosso1@gmail.com>
2021-01-13 22:45:13 +0300

    3.2. Зачем нужно приведение типов

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,27 +1,32 @@
 package ru.job4j.poly;
 
-public class Bus implements Transport{
-    @Override
-    public void go() {
-        int pointa = 23;
-        int pointb = 25;
-        int allway = pointa + pointb;
-    }
+public class Bus implements Vehicle{
+//    @Override
+//    public void go() {
+//        int pointa = 23;
+//        int pointb = 25;
+//        int allway = pointa + pointb;
+//    }
+//
+//    @Override
+//    public void passengers(int temp) {
+//         int exita = 10;
+//         int exitb = 5;
+//         int allexit = exita + exitb;
+//    }
+//
+//    @Override
+//    public int fillup(int full) {
+//        int price;
+//        int fulla = 12;
+//        int fullb = 10;
+//        int cost = 2;
+//        int allfull = fulla + fullb;
+//        return price = allfull * cost;
+//    }
 
     @Override
-    public void passengers(int temp) {
-         int exita = 10;
-         int exitb = 5;
-         int allexit = exita + exitb;
-    }
-
-    @Override
-    public int fillup(int full) {
-        int price;
-        int fulla = 12;
-        int fullb = 10;
-        int cost = 2;
-        int allfull = fulla + fullb;
-        return price = allfull * cost;
+    public void move() {
+        System.out.println(getClass().getName() + " едет по дороге");
     }
 }
