8424e731d92b02cca572976ea8bd528d77e2a759
Сергей <seirosso1@gmail.com>
2021-01-27 23:38:30 +0300

    2. Очередь с приоритетом на LinkedList

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,14 +8,16 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() > element.getPriority() && task.getPriority() < element.getPriority()) {
-                index++;
+            if (task.getPriority() < element.getPriority()) {
+                break;
             }
-            this.tasks.add(index, task);
+            index++;
+
         }
+        this.tasks.add(index, task);
     }
 
-        public Task take () {
-            return tasks.poll();
-        }
+    public Task take() {
+        return tasks.poll();
+    }
 }
