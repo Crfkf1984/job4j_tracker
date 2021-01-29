4d9e61c314a8519932efe779b0ef95483757ca57
Сергей <seirosso1@gmail.com>
2021-01-29 23:25:48 +0300

    0.5. Иерархия исключений и множественный catch

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -13,7 +13,7 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if ((user.isValid()) && (user.getUsername().length() >= 3))  {
+        if ((user.isValid()) || (user.getUsername().length() >= 3))  {
         return true;
     }
      throw new UserInvalidException("Пользователь не валидный!");
