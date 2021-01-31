4ff35c25acfcba674aaacf90f65996b0d68218f7
Сергей <seirosso1@gmail.com>
2021-01-31 09:43:49 +0300

    2. Singleton

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -2,7 +2,19 @@ package ru.job4j.tracker;
 
 import java.util.Arrays;
 
-public class Tracker {
+public final class Tracker {
+    private static Tracker instance = null;
+
+    private Tracker() {
+
+    }
+
+    public Tracker getInstance() {
+        if (instance == null) {
+            instance = new Tracker();
+        }
+        return instance;
+    }
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
