4d83c34fc2995b7ff0f7da79d2b9a7bf9d3d751e
Сергей <seirosso1@gmail.com>
2021-02-02 22:22:37 +0300

    1. Уникальные задачи

---
+++ src/main/java/ru/job4j/collection/Task.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+import java.util.List;
+
+public class Task {
+    private String number;
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Task(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
