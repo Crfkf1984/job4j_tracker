fcb9264774cb42e2ea52f3c8601d1a12b5bd6d24
Сергей <seirosso1@gmail.com>
2021-02-04 22:11:24 +0300

    0. Map, HashMap

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("sergey@gmail.com", "Kotovich Sergey Borisovich");
+        for (String key : hashMap.keySet()) {
+            String value = hashMap.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
