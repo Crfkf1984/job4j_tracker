8ea3f8b2b0bdebd8f905329ac452f88daf61c9bc
Сергей <seirosso1@gmail.com>
2021-02-08 20:17:03 +0300

    0. Сортировка

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,7 +1,11 @@
 package ru.job4j.tracker;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-public class Item {
+public class Item implements Comparator<Item> {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
@@ -54,5 +58,20 @@ public class Item {
                 ", name='" + name + '\'' +
                 ", created=" + created +
                 '}';
+    }
+
+    @Override
+    public int compare(Item o1, Item o2) {
+        return 0;
+    }
+
+    public static List<Integer> vozrastanie(List<Integer> item) {
+        Collections.sort(item);
+        return item;
+    }
+
+    public static List<Integer> ubivanie(List<Integer> item) {
+        Collections.sort(item, Collections.reverseOrder());
+        return item;
     }
 }
