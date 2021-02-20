43bbc6b24bacbccd02dab14b4217bc4c29fd7f0e
Сергей <seirosso1@gmail.com>
2021-02-20 21:08:15 +0300

    2.2. Лямбда блок

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,19 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("image 1", 100),
+                new Attachment("image 2", 34),
+                new Attachment("image 3", 13)
+        );
+        Comparator<Attachment> comparator = (left, right) -> {
+            System.out.println("compare - " + left.getName() + " : " + right.getName());
+            return right.getName().compareTo(left.getName());
+        };
+    }
+}
